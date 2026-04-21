7dd69f97740208d6288c3bff4f768e43a5892460
Ronith Jean (RA2511026011077) <meronijean@gmail.com>
2026-04-21 13:53:13 +0530

    Add files via upload

---
+++ UC4.java
@@ -0,0 +1,15 @@
+public class SlotToIndex {
+	public static int[] convertSlot(int slot) {
+		int index = slot-1;
+		int row=index/3;
+		int col=index%3;
+		return new int[]{row,col};
+	}
+	public static void main(String[] args){
+		int slot = 5;
+		int[] result = convertSlot(slot);
+		System.out.println("Slot: " + slot);
+        System.out.println("Row: " + result[0]);
+        System.out.println("Column: " + result[1]);
+	}
+}
